a1c421e5387c82fb22484eccf8ab6277f6e7141a
PercivalJ Loyola <percivalloyal@gmail.com>
2015-10-18 14:44:02 -0700

    adding User server

--- api/app.js
+++ api/app.js
@@ -1,25 +1,29 @@
-var express 		= require('express');
-var path 			= require('path');
-var cors 			= require('cors');
-var logger 			= require('morgan');
-var bodyParser 		= require('body-parser');
-var app 			= express();
+var express 		= require('express')
+var path 			= require('path')
+var cors 			= require('cors')
+var logger 			= require('morgan')
+var bodyParser 		= require('body-parser')
+var app 			= express()
+var morgan			= require('morgan')
+var port 			= process.env.PORT || 8080
+var mongoose 		= require('mongoose')
+var apiRouter = require('./api/config/userRoutes')
 
-var mongoose 		= require('mongoose');
+mongoose.connect('mongodb://localhost:27017/golunch')
 
-mongoose.connect('mongodb://localhost:27017/golunch');
+var routes 			= require('./config/routes')
 
-var routes 			= require('./config/routes');
 
-app.use(cors());
+// set up middleware
+app.use(cors())
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/api', routes);
+app.use('/api', routes)
 
-app.listen(8080);
+app.listen(port)
 
-console.log("running the server on port 8080! omg magic!")
+console.log("omg magic! running the server on " + port)
 
